a23d29d2a4c6c5227f5cfd25dcbd74d1537d3555
NCarlisle24 <lorenzonolan12983@gmail.com>
2026-02-21 22:45:55 -0700

    removed Amplify import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import ReactDOM from "react-dom/client"
 import router from "./system.tsx"
 import { RouterProvider } from "react-router"
-import { Amplify } from "aws-amplify";
+// import { Amplify } from "aws-amplify";
 // import amplifyOutputs from "../amplify_outputs.json";
 // 
 // Amplify.configure(amplifyOutputs);
